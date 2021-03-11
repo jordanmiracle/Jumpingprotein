3854b341ce5ea77123467628868583f9fa8e0906
Jordan Miracle <jordanmiracle@protonmail.com>
2021-03-10 19:43:38 -0800

    imported os and created the staticfiles-dirs and pointed it.

--- jumpingprotein/urls.py
+++ jumpingprotein/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from webapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webapp/', include('jumpingprotein.urls')),
+    path('', views.index, name='index'),
 ]
+
+
+
