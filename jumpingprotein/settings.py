a4c2de325bc7c2b17fed1304eac1ab4accd21dc2
Jordan Miracle <jordanmiracle@protonmail.com>
2021-03-13 00:42:30 -0800

    Built out the database and connection. Also, added secret key for database password.

--- jumpingprotein/settings.py
+++ jumpingprotein/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +23,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '*9s9dl#1fnc9s7&78w73%l#ri^7lyr$%k*&m3jdyq1m!7x9#mt'
+
+import json
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
+
+
+def get_secret(setting, secrets=secrets):
+    """Get secret setting or fail with ImproperlyConfigured"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,10 +98,14 @@ WSGI_APPLICATION = 'jumpingprotein.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'USER': 'jordanmiracle',
+       'NAME': 'jpblogdb',
+       'HOST': 'localhost',
+       'PASSWORD': get_secret('DB_PASSWORD'),
+       'PORT': '',
+   },
 }
 
 FIXTURE_DIRS = [
