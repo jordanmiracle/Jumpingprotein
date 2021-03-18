06434c489cb51ea69e78dbf894f0dd0061725ad4
Jordan Miracle <jordanmiracle@protonmail.com>
2021-03-17 19:38:12 -0700

    Minor changes.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import (Paginator, EmptyPage, PageNotAnInteger)
+from django.views.generic import ListView, DetailView
 
 
 def post_list(request):
