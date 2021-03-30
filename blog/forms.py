c2c854d6fe48455eff23ab96a744f9e9038d1e92
jordanmiracle <jordanmiracle@protonmail.com>
2021-03-30 02:42:13 -0700

    Got rid of unnecessary things, and added AddPostView.

--- blog/forms.py
+++ blog/forms.py
@@ -1,45 +1,28 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post
+
+
 # choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment'),]
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
+        fields = ('title', 'author', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'esnath', 'type': 'hidden'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
-            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+            'body': forms.Textarea(attrs={'class': 'form-control'})}
 
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body', 'snippet')
+        fields = ('title', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
-            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-        }
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('name', 'body')
-
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
-
-        }
-
+            'body': forms.Textarea(attrs={'class': 'form-control'})}
