6c77a478ff4b08681d37661a1104b710da55a75e
Jordan Miracle <jordanmiracle@protonmail.com>
2021-09-13 17:09:47 -0700

    changed s3 media settings back to what they were

--- jumpingprotein/settings.py
+++ jumpingprotein/settings.py
@@ -260,7 +260,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 ##############################################
