99cf22bdcab00bc2c986d854a9a96deee0537e01
miracle <jordanmiracle@protonmail.com>
2021-09-26 01:44:50 -0700

    removed the html minifyer in settings

--- jumpingprotein/settings.py
+++ jumpingprotein/settings.py
@@ -81,7 +81,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.gzip.GZipMiddleware',
-    'htmlmin.middleware.HtmlMinifyMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
