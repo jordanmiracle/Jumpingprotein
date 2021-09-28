9c13f6d02124834ca87ce1066e1e0c718f472137
miracle <jordanmiracle@protonmail.com>
2021-09-28 03:22:29 -0700

    added settings for setting up a cache

--- jumpingprotein/settings.py
+++ jumpingprotein/settings.py
@@ -338,27 +338,52 @@ if not DEBUG:
 
 ########### Memcache / Compression ##############
 
-# CACHES = {
-#    'default': {
-#        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#        'LOCATION': '127.0.0.1:11211',
-#    }
-# }
-#
-# COMPRESS_ENABLED = True
-# COMPRESS_CSS_HASHING_METHOD = 'content'
-# COMPRESS_FILTERS = {
-#    'css':[
-#        'compressor.filters.css_default.CssAbsoluteFilter',
-#        'compressor.filters.cssmin.rCSSMinFilter',
-#    ],
-#    'js':[
-#        'compressor.filters.jsmin.JSMinFilter',
-#    ]
-# }
-# KEEP_COMMENTS_ON_MINIFYING = True
-#
-# EXCLUDE_FROM_MINIFYING = ('^articles/', '^photos' '^admin/')
+servers = os.environ['MEMCACHIER_SERVERS']
+username = os.environ['MEMCACHIER_USERNAME']
+password = os.environ['MEMCACHIER_PASSWORD']
+
+CACHES = {
+    'default': {
+        # Use pylibmc
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+
+        'LOCATION': servers,
+
+        'OPTIONS': {
+            # Use binary memcache protocol (needed for authentication)
+            'binary': True,
+            'username': username,
+            'password': password,
+            'behaviors': {
+                # Enable faster IO
+                'no_block': True,
+                'tcp_nodelay': True,
+
+                # Keep connection alive
+                'tcp_keepalive': True,
+
+                # Timeout settings
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
+
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30,
+            }
+        }
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
 
 # STATICFILES_FINDERS = (
