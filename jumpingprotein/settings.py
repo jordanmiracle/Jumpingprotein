2e23ffde956aa99987299d5afebc4ced7e5d5be9
miracle <jordanmiracle@protonmail.com>
2021-09-28 03:41:01 -0700

    added django-heroku-memcacheify instead of everything else. deleted the rest

--- jumpingprotein/settings.py
+++ jumpingprotein/settings.py
@@ -338,52 +338,9 @@ if not DEBUG:
 
 ########### Memcache / Compression ##############
 
-servers = os.environ['MEMCACHIER_SERVERS']
-username = os.environ['MEMCACHIER_USERNAME']
-password = os.environ['MEMCACHIER_PASSWORD']
+from memcacheify import memcacheify
 
-CACHES = {
-    'default': {
-        # Use pylibmc
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-
-        # TIMEOUT is not the connection timeout! It's the default expiration
-        # timeout that should be applied to keys! Setting it to `None`
-        # disables expiration.
-        'TIMEOUT': None,
-
-        'LOCATION': servers,
-
-        'OPTIONS': {
-            # Use binary memcache protocol (needed for authentication)
-            'binary': True,
-            'username': username,
-            'password': password,
-            'behaviors': {
-                # Enable faster IO
-                'no_block': True,
-                'tcp_nodelay': True,
-
-                # Keep connection alive
-                'tcp_keepalive': True,
-
-                # Timeout settings
-                'connect_timeout': 2000, # ms
-                'send_timeout': 750 * 1000, # us
-                'receive_timeout': 750 * 1000, # us
-                '_poll_timeout': 2000, # ms
-
-                # Better failover
-                'ketama': True,
-                'remove_failed': 1,
-                'retry_timeout': 2,
-                'dead_timeout': 30,
-            }
-        }
-    }
-}
-
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+CACHES = memcacheify()
 
 
 # STATICFILES_FINDERS = (
@@ -392,5 +349,5 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 #        'compressor.finders.CompressorFinder',
 # )
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-COMPRESS_ROOT = STATIC_ROOT
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+#COMPRESS_ROOT = STATIC_ROOT
--- webapp/views.py
+++ webapp/views.py
@@ -1,22 +1,11 @@
-from django.conf import settings
-from django.http import HttpResponse
-from compression_middleware.decorators import compress_page
-from django.views.decorators.gzip import gzip_page
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
-from django.utils.cache import learn_cache_key
-
-timeout = 600
-view_keys = {}
 
 
-@cache_page(None)
+
 def index(request):
-    response = render('webapp/index.html', {})
-    view_keys['index'] = learn_cache_key(request, response)
-    return response
+    return render(request, 'webapp/index.html', {})
 
 
-@cache_page(None)
+
 def about(request):
     return render(request, 'webapp/about.html', {})
