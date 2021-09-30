933c2f60e5c569af7ef8287ba9131c19992eca68
miracle <jordanmiracle@protonmail.com>
2021-09-30 03:46:58 -0700

    again, small changes to the compress for css. May try more because it seems to be working. Also added a google translate button.

--- jumpingprotein/settings.py
+++ jumpingprotein/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'pwa',
     'django_cleanup',
     'compressor',
+    'sorl.thumbnail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
